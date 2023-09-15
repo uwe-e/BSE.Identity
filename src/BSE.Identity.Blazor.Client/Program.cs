3c45e017bf3a38a14c9049645dfe7a65b5825b53
Uwe Eichkorn <git.bsetunes@outlook.com>
2023-09-15 10:55:19 +0200

    globalization added

--- src/BSE.Identity.Blazor.Client/Program.cs
+++ src/BSE.Identity.Blazor.Client/Program.cs
@@ -24,7 +24,19 @@ builder.Services.AddHttpClient();
 
 builder.Services.AddFluentUIComponents();
 
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+
+
 var app = builder.Build();
+
+//var supportedCultures = new[] { "en-US", "de-de" };
+//var localizationOptions = new RequestLocalizationOptions()
+//    .SetDefaultCulture(supportedCultures[0])
+//    .AddSupportedCultures(supportedCultures)
+//    .AddSupportedUICultures(supportedCultures);
+
+//app.UseRequestLocalization(localizationOptions);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
