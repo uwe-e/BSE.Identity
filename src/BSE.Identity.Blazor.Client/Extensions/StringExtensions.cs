12c51b5bf3fdf817d25eae0335fa5f567f70919e
Uwe Eichkorn <git.bsetunes@outlook.com>
2023-10-13 14:28:11 +0200

    navigationbar added

---
+++ src/BSE.Identity.Blazor.Client/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace BSE.Identity.Blazor.Client
+{
+    public static class StringExtensions
+    {
+        public static string ToUpperFirstChar(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return string.Empty;
+            }
+
+            return $"{input[0].ToString().ToUpper()}{input.Substring(1)}";
+        }
+    }
+}
