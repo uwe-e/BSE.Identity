e5719058f1c993519cc4f703af1d225da57a3da2
Uwe Eichkorn <git.bsetunes@outlook.com>
2023-10-15 10:23:07 +0200

    rolemanagement added

---
+++ src/BSE.Identity.Blazor.Client/ViewModels/RoleViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BSE.Identity.Blazor.Client.ViewModels
+{
+    public class RoleViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
