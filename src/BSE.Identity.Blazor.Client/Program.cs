da141736170f83d553d69cb9eaeab7fa1c0083c5
Uwe Eichkorn <git.bsetunes@outlook.com>
2026-06-15 11:23:38 +0200

    Microsoft.Fast.Components.FluentUI replacement

--- src/BSE.Identity.Blazor.Client/Program.cs
+++ src/BSE.Identity.Blazor.Client/Program.cs
@@ -7,7 +7,7 @@ using BSE.Identity.Blazor.Client.Services;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Fast.Components.FluentUI;
+using Microsoft.FluentUI.AspNetCore.Components;
 using MySqlConnector;
 using System.Security.Cryptography.X509Certificates;
 
@@ -42,7 +42,7 @@ var connectionStringBuilder = new MySqlConnectionStringBuilder
 };
 
 builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
-    options.UseMySql(connectionStringBuilder.ConnectionString, new MySqlServerVersion(new Version(8, 0, 43)))
+    options.UseMySql(connectionStringBuilder.ConnectionString, ServerVersion.AutoDetect(connectionStringBuilder.ConnectionString))
     );
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
